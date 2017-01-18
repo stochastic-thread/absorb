54065fb67925f386fe85403072daeb3256c77854
Arthur Colle <hello@arthurcolle.com>
2017-01-18 03:49:30 -0500

    First commit.

---
+++ lib/absorb.ex
@@ -0,0 +1,18 @@
+defmodule Absorb do
+  @moduledoc """
+  Documentation for Absorb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Absorb.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
